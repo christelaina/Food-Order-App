30578bc18a7d09b00ae64086fc1a27110063e593
elaina <gea.christensen@gmail.com>
2022-06-07 23:55:53 -0400

    adding header, cart button, and meals component, and displaying meal items

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,55 @@
+import Card from '../UI/Cart';
+import MealItem from './MealItem/MealItem';
+import classes from './AvailableMeals.module.css';
+
+const DUMMY_MEALS = [
+  {
+    id: 'm1',
+    name: 'KAISEKI ZENSAI',
+    description:
+      'Hokkaido scallop crudo, Ikura, dashi glaze saikyo roasted sablefish, wild garlic miso, tomato jam,charred octopus kabayaki, sea urchin, amazu jelly.',
+    price: 22.99,
+  },
+  {
+    id: 'm2',
+    name: 'Pan-Roasted Beef Tenderloin',
+    description:
+      'carrot kombu purée, king oyster mushrooms, braised red cabbage, Japanese peppercorn jus, black garlic miso foie gras+ upgrade to American Wagyu for $35',
+    price: 16.5,
+  },
+  {
+    id: 'm3',
+    name: 'Sushi Selection',
+    description:
+      'Aburi salmon oshi, Aburi ebi oshi, Aburi yellowtail oshi, Japanese tai nigiri, bluefin tuna nigiri.',
+    price: 12.99,
+  },
+  {
+    id: 'm4',
+    name: 'WholeFruit Dark Chocolate Cake',
+    description:
+      'strawberry raspberry sorbet, strawberry jelly, raspberry coulis, genmai tea caramel tuile.',
+    price: 18.99,
+  },
+];
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
