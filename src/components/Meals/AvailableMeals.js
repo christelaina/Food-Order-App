000c616ff6010edabf0ca9b2043765b1dfc96b06
elaina <gea.christensen@gmail.com>
2022-06-11 23:08:25 -0400

    moving meal data to the backend(firebase)

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,40 +1,33 @@
-import Card from '../UI/Cart';
+import { useEffect, useState } from 'react';
+import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'KAISEKI ZENSAI',
-    description:
-      'Hokkaido scallop crudo, Ikura, dashi glaze saikyo roasted sablefish, wild garlic miso, tomato jam, charred octopus kabayaki, sea urchin, amazu jelly.',
-    price: 24,
-  },
-  {
-    id: 'm2',
-    name: 'Pan-Roasted Beef Tenderloin',
-    description:
-      'carrot kombu purée, king oyster mushrooms, braised red cabbage, Japanese peppercorn jus, black garlic miso foie gras. + upgrade to American Wagyu for $35',
-    price: 35,
-  },
-  {
-    id: 'm3',
-    name: 'Sushi Selection',
-    description:
-      'Aburi salmon oshi, Aburi ebi oshi, Aburi yellowtail oshi, Japanese tai nigiri, bluefin tuna nigiri.',
-    price: 21,
-  },
-  {
-    id: 'm4',
-    name: 'WholeFruit Dark Chocolate Cake',
-    description:
-      'strawberry raspberry sorbet, strawberry jelly, raspberry coulis, genmai tea caramel tuile.',
-    price: 14.5,
-  },
-];
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        'https://reacteats-2df96-default-rtdb.firebaseio.com/meals.json'
+      );
+      const responseData = await response.json(); //returns an object but we want an array
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+    };
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
